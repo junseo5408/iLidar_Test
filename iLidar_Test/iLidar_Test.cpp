efd2a40608cc65e57d9b6da7d12cd6b09791a3e7
hwang junseo <junseo5408@gmail.com>
2025-09-25 06:05:50 +0900

    라즈베리파이 연결 코드 추가

--- iLidar_Test/iLidar_Test.cpp
+++ iLidar_Test/iLidar_Test.cpp
@@ -6,8 +6,12 @@
 #include <queue>
 #include <iostream>
 
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
 // Include iLiDAR library
 #include "../src/ilidar.hpp"
+using namespace std;
 
 // 글로벌 데이터 버퍼
 static iTFS::img_t lidar_img_data[iTFS::max_device];
@@ -40,7 +44,30 @@ static void info_packet_handler(iTFS::device_t* device) {
     printf("[INFO] LiDAR #%d SN %d ready\n", device->idx, device->info.sensor_sn);
 }
 
+
+void sendSignalToRaspberryPI(const char* ip, int port, const char* msg) {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed\n";
+        return;
+    }
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &addr.sin_addr);
+
+    sendto(sock, msg, strlen(msg), 0, (sockaddr*)&addr, sizeof(addr));
+
+    closesocket(sock);
+    WSACleanup();
+} 
+
 int main(int argc, char* argv[]) {
+    int port = 8080;
+
     // LiDAR 객체 생성
     iTFS::LiDAR* ilidar = new iTFS::LiDAR(
         lidar_data_handler,
@@ -68,6 +95,13 @@ int main(int argc, char* argv[]) {
             printf("%d ", lidar_img_data[idx].img[0][i]);
         }
         printf("\n");
+
+        if (lidar_img_data[idx].img[0][0] < 1000) { // 1m 이내 물체 감지
+            sendSignalToRaspberryPI("192.168.0.50", port, "LED_ON");
+        }
+        else {
+            sendSignalToRaspberryPI("192.168.0.50", port, "LED_OFF");
+        }
     }
 
     delete ilidar;
